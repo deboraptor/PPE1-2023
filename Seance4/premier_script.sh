01ed7f948e637639cd162e29ca3c8b05be86ce37
Débora <vdzdebora@gmail.com>
2023-10-15 22:30:41 +0200

    Ajout de mon scipt de la séance 4

---
+++ Seance4/premier_script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# On crée 4 variables avec =$ avec les emplacements des fichiers et les commandes 
+nb_locations_2016=$(cat ../Fichiers/ann/2016/*/* | egrep "Location" | cut -f 3 | wc -l)
+nb_locations_2017=$(cat ../Fichiers/ann/2017/*/* | egrep "Location" | cut -f 3 | wc -l)
+nb_locations_2018=$(cat ../Fichiers/ann/2018/*/* | egrep "Location" | cut -f 3 | wc -l)
+nb_locations_all=$(cat ../Fichiers/ann/*/*/* | egrep "Location" | cut -f 3 | wc -l)
+
+# Avec echo on affiche les résultats
+echo "Les locations pour l'année 2016 sont : $nb_locations_2016"
+echo "Les locations pour l'année 2017 sont : $nb_locations_2017"
+echo "Les locations pour l'année 2018 sont : $nb_locations_2018"
+echo "Les locations pour les trois années sont : $nb_locations_all"
+
